8825c8e921f22e544feb7724eb54b373cdaa76c8
KILLER42 <40739278+KILLER42@users.noreply.github.com>
2024-05-02 04:56:05 +0300

    added files

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// Задание 1
+const arrowFunc1 = (a, b) => a + b;
+console.log(arrowFunc1(1, 2));
+
+// Задание 2
+const arrowFunc2 = (str) => str.length;
+console.log(arrowFunc2('Some string'));
+
+// Задание 3
+const arrowFunc3 = (arr) => arr.map((el) => el + 1);
+console.log(arrowFunc3([1, 2, 3, 4]));
